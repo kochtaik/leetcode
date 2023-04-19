2d7fee1b034ce4b1f4fee7fc882810cb8d718ea3
evgeny.padlesny <evgeny.padlesny@cibt.com>
2023-04-19 11:10:36 +0200

    Solve "344. Reverse string" in-place recursively

--- recursion/344_reverse-string.js
+++ recursion/344_reverse-string.js
@@ -1,6 +1,6 @@
 /**
  * @param {string[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * @return {void} 
  */
 function reverseStringNotInPlace(s) {
   return s.length ? [...reverseStringNotInPlace(s.slice(1)), ...s.slice(0,1)] : '';
@@ -10,7 +10,7 @@ function reverseStringNotInPlace(s) {
  * @param {string[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-function reverseString(s) {
+function reverseStringIteratively(s) {
   let i = 0;
   let j = s.length - 1;
 
@@ -22,6 +22,18 @@ function reverseString(s) {
   }
 }
 
+/**
+ * @param {string[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(s, i = 0, j = s.length - 1) {
+  if (i > j) return;
+  const tmp = s[i];
+  s[i] = s[j];
+  s[j] = tmp;
+  reverseString(s, i + 1, j - 1)
+}
+
 const s = ["r","o","c","k"];
-reverseString(s)
-console.log(s)
+reverseString(s);
+console.log(s);
